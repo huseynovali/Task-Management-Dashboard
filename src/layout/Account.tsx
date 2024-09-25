79a1150b38b0ae9561d3ce61840225986667ea60
User <huseynovali612@gmail.com>
2024-09-25 20:56:16 +0400

    config accound

--- src/layout/Account.tsx
+++ src/layout/Account.tsx
@@ -1,13 +1,25 @@
-import user from '../assets/Profil.png'
+import user from "../assets/Profil.png";
+import notification from "../assets/Notification.png";
 function Account() {
   return (
-    <div className='absolute top-0 right-0 py-8'>
-      <div className="left"></div>
-      <div className="right rounded-full w-[52px] h-[52px]">
+    <div className="absolute flex items-center gap-x-5 top-0 right-0 py-8 px-8">
+      <div className="left">
+        <div
+          className="
+        w-[52px] h-[52px] border rounded-full flex justify-center items-center
+         border-[#F5F5F7] cursor-pointer hover:border-[#dddddf] transition-colors"
+        >
+          <div className="relative">
+            <div className="absolute w-2 h-2 bg-[#FF4D5E] rounded-full right-0"></div>
+            <img src={notification} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="right rounded-full w-[52px] h-[52px] cursor-pointer border hover:border-[#dddddf]">
         <img src={user} alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
