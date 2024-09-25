44aab6125b83f49d4eb2677cb533ee526fa12e19
User <huseynovali612@gmail.com>
2024-09-26 01:13:27 +0400

    create calendar

---
+++ src/components/WeeklyCalendar.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { addDays, startOfWeek } from "date-fns";
+
+function WeeklyCalendar() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const start = startOfWeek(currentDate, { weekStartsOn: 0 });
+  const daysOfWeek = Array.from({ length: 7 }).map((_, index) =>
+    addDays(start, index)
+  );
+
+  const formatDate = (date) => {
+    return new Intl.DateTimeFormat("en-US", { day: "numeric" }).format(date);
+  };
+
+  return (
+    <div className="p-5 bg-white rounded-md">
+      <div className="w-full mb-5 flex justify-between items-center px-2">
+        <button
+          onClick={() => setCurrentDate(addDays(currentDate, -7))}
+          className="text-xl"
+        >
+          {"<"}
+        </button>
+        <h2 className="text-sm font-semibold">
+          {new Intl.DateTimeFormat("en-US", {
+            month: "long",
+            year: "numeric",
+          }).format(currentDate)}
+        </h2>
+        <button
+          onClick={() => setCurrentDate(addDays(currentDate, 7))}
+          className="text-xl"
+        >
+          {">"}
+        </button>
+      </div>
+      <div className="calendar-week">
+        {daysOfWeek.map((date) => (
+          <div
+            key={date}
+            className="calendar-day"
+            onClick={() => setCurrentDate(date)}
+          >
+            <div
+              className={`flex flex-col items-center  rounded-3xl p-1 transition-all  ${
+                date.getDate() === currentDate.getDate()
+                  ? "bg-[#141522] text-white"
+                  : ""
+              }`}
+            >
+              <span className="text-xs font-medium mb-3">
+                {
+                  date
+                    .toLocaleDateString("en-US", { weekday: "short" })
+                    .split("")[0]
+                }
+              </span>
+              <span
+                className={` h-[32px] w-[32px] rounded-full text-xs font-medium flex justify-center items-center transition-all ${
+                  date.getDate() === currentDate.getDate()
+                    ? "bg-[#546FFF] text-white"
+                    : "bg-[#F5F5F7]"
+                }`}
+              >
+                {formatDate(date)}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default WeeklyCalendar;
--- src/components/sidebar/OwerviewSidear.tsx
+++ src/components/sidebar/OwerviewSidear.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import WeeklyCalendar from '../WeeklyCalendar'
 
 function OwerviewSidear() {
   return (
-    <div>OwerviewSidear</div>
+    <div className='p-5 bg-[#F5F5F7]'>
+      <WeeklyCalendar />
+    </div>
   )
 }
 
